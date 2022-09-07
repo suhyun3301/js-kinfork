b3260bdf9f5a5faf4766603618b944d8f4ce43f6
suhyun3301 <iam3301@naver.com>
2022-09-07 22:52:25 +0900

    ✨ our-shop : tiny-slider 작업 중

---
+++ main-slider.js
@@ -0,0 +1,12 @@
+const slider = tns({
+  container: '.slide-card-box .slide-card-list',
+  slideBy: 'page',
+  items: 1,
+  autoplay: false,
+  mouseDrag: true,
+  // center: true,
+  speed: 400,
+  gutter: 10,
+  edgePadding: 50,
+  rewind: true,
+})
